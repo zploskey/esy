43ea673f9e13cfa7d35b96688df5b44ea8f7383e
Bryan Phelps <bryphe@outlook.com>
2018-07-19 12:25:55 -0700

    Try using a non-root directory for esy home

---
+++ scripts/bootstrap/Test-Build-Project.ps1
@@ -0,0 +1,33 @@
+# Windows initial test script
+# This is just a VERY basic test exercising `esy install` and `esy build` for a canonical project:
+# `esy-reason-project`
+#
+# This should be removed once we have the windows build hooked up to our existing test suite
+
+function exitIfFailed() {
+    if ($LastExitCode -ne 0) {
+        exit $LastExitCode
+    }
+}
+
+Write-Host "Cloning test repo.."
+git clone https://github.com/esy-ocaml/esy-reason-project C:/erp
+exitIfFailed
+Write-Host "Clone complete!"
+cd C:/erp
+ls
+
+mkdir C:/projects/.esy-home
+$env:HOME="C:/projects/.esy-home"
+
+# "Integration Test" for now
+# This requires retries on all platforms at the moment:
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe install
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe install
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe install
+
+exitIfFailed
+
+# TODO: Bring this back when we have a project that can build successfully!
+# C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe build
+# exitIfFailed
